f53216b5a3b743d1a5084c2fe2e3f67b45a71288
HalkoTami <halko316hvb@gmail.com>
2023-06-03 14:38:58 +0900

    Initial Commit

---
+++ ShortcutterPro/ShortcutterProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcutterProApp.swift
+//  ShortcutterPro
+//
+//  Created by Haruko Tamamushi on 2023/06/03.
+//
+
+import SwiftUI
+
+@main
+struct ShortcutterProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
